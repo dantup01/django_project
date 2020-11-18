eb8b346fc4a08b3fd61ccaf03bc20090f3468d4c
dantup01 <daniel.tuppen1@gmail.com>
2020-11-18 11:17:28 +0000

    uploading django project

---
+++ django_project/blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('new_arrivals/', views.new_arrivals, name='new_arrivals'),
+    path('men/', views.men, name='men'),
+    path('women/', views.women, name='women'),
+]
